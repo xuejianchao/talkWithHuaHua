764efefa5c114d3b4287c2048840fb253c2dea3c
xuejianchao <xuejianchao@gmail.com>
2019-06-18 15:17:57 +0800

    half done msg bubble component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <msgBubble v-bind:which-side="whichSide"
+      v-bind:content="msgContent"></msgBubble>
+  </div>
+</template>
+
+<script>
+import msgBubble from "./components/msgBubble.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      whichSide: "right",
+      msgContent:
+        "照道理说，他也不能回到故国去，或许已经死在半路上了吧？可能再也不会听到关于那家伙的事情了。"
+    };
+  },
+  components: {
+    msgBubble
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/msgBubble.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-bind:class="classObject">
+    {{content}}
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  props: ["whichSide", "content"],
+  computed: {
+    classObject: function() {
+      return {
+        msgBubble: true,
+        leftSide: this.whichSide === "left",
+        rightSide: this.whichSide === "right"
+      };
+    }
+  }
+};
+</script>
+
+<style>
+.msgBubble {
+  padding: 0.8rem;
+  border-radius: 0.5rem;
+  width: fit-content;
+}
+
+.leftSide {
+  background-color: #ffffff;
+  border-radius: 0 0.5rem 0.5rem 0.5rem;
+}
+
+.rightSide {
+  background-color: #cfd8dc;
+  border-radius: 0.5rem 0 0.5rem 0.5rem;
+}
+</style>
+
